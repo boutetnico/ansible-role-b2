94ce2d032d1ec1acf7ee1cd6afb148952d4e7d16
Nicolas Boutet <amd3002@gmail.com>
2023-06-28 10:04:16 +0200

    Remove support for Debian Buster, add support for Debian Bookworm

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -16,5 +16,4 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ],
 )
 def test_package_is_installed(host, name):
-    packages = host.pip.get_packages(pip_path="pip3")
-    assert name in packages
+    assert host.package(name).is_installed

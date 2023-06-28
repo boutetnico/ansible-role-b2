a4f849ef44618623572689099b3a90e255e5f589
Nicolas Boutet <amd3002@gmail.com>
2023-06-28 10:28:25 +0200

    Fix test

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -19,7 +19,7 @@ def test_package_is_installed(host, name):
     assert host.package(name).is_installed
 
 
-def test_b2_command_can_be_ran(host, user, roles):
+def test_b2_command_can_be_ran(host):
     output = host.run("b2 version")
     if output.exit_status != 0:
         pytest.fail("Failed to run b2")

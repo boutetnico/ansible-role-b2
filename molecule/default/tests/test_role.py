9497309bae15fd897f4ac0a9dcdfde2c420e8774
Nicolas Boutet <amd3002@gmail.com>
2023-06-28 14:39:02 +0200

    Change test

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -19,8 +19,5 @@ def test_package_is_installed(host, name):
     assert host.package(name).is_installed
 
 
-def test_b2_command_can_be_ran(host):
-    output = host.run("b2 version")
-    if output.exit_status != 0:
-        pytest.fail("Failed to run b2")
-    assert "b2 command line too" in output.stdout
+def test_b2_command_exists(host):
+    assert host.exists("b2")
